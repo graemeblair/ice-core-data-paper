0de1860086b669ee03f480ee8c0f1908c35ed286
Phil Neff <philneff@uw.edu>
2025-12-05 16:52:06 -0800

    Updated figures with appropriate styling

--- code/validation-plots.R
+++ code/validation-plots.R
@@ -5,6 +5,10 @@ library(data.table)
 library(here)
 library(ggVennDiagram)
 
+# ---- Options ----
+
+options(scipen = 1000000)
+
 # ---- Read in to temporary file ----
 
 # read in ICE Core data
@@ -53,6 +57,8 @@ p1 <- ggVennDiagram(x = sets,
 
 ggsave(filename=here("figures", "id-upset.pdf"), plot=p1, dpi=300, width=8, height=4, units="in")
 
+# We hand-edit "set size" x axis labels to fix overlap in resulting figure
+
 # ---- Plot of weekly enforcement actions ----
 
 dat1 <- encounters %>% 
@@ -60,63 +66,57 @@ dat1 <- encounters %>%
          event_date <= "2025-07-27") %>% 
   mutate(week = floor_date(event_date, "week", week_start = "Monday")) %>% 
   count(week) %>% 
-  mutate(type = "encounters")
+  mutate(type = "Encounters")
 
 dat2 <- detainers %>% 
   filter(detainer_prepare_date >= "2023-09-04",
          detainer_prepare_date <= "2025-07-27") %>% 
   mutate(week = floor_date(detainer_prepare_date, "week", week_start = "Monday")) %>% 
   count(week) %>% 
-  mutate(type = "detainers")
+  mutate(type = "Detainers")
 
 dat3 <- arrests %>% 
   filter(apprehension_date >= "2023-09-04",
          apprehension_date <= "2025-07-27") %>% 
   mutate(week = floor_date(apprehension_date, "week", week_start = "Monday")) %>% 
   count(week) %>% 
-  mutate(type = "arrests")
+  mutate(type = "Arrests")
 
 dat4 <- detention_stays %>% 
   filter(stay_book_in_date_time >= "2023-09-04",
          stay_book_in_date_time <= "2025-07-27") %>% 
   mutate(week = floor_date(stay_book_in_date_time, "week", week_start = "Monday")) %>% 
   count(week) %>% 
-  mutate(type = "detentions")
+  mutate(type = "Detentions")
 
 dat5 <- removals %>% 
   filter(departed_date >= "2023-09-04",
          departed_date <= "2025-07-27") %>% 
   mutate(week = floor_date(departed_date, "week", week_start = "Monday")) %>% 
   count(week) %>% 
-  mutate(type = "removals")
+  mutate(type = "Removals")
 
 dat <- rbind(dat1, dat2, dat3, dat4, dat5)
 
+levels = c("Encounters", "Detainers", "Arrests", "Detentions", "Removals")
+
 p2 <- dat %>% 
+  mutate(type = factor(type, levels=levels)) %>% 
   ggplot(aes(x = week, y = n, color = type)) +
   geom_line() +
   ylim(0, NA) +
-  labs(title = "Weekly enforcement events & detention stay book-ins")
+  scale_color_viridis_d() +
+  # change y axis scale to thousands
+  scale_y_continuous(labels = scales::comma) +
+  # make x axis date format month-year
+  scale_x_date(date_labels = "%b.\n%Y", date_breaks = "6 months") + 
+  labs(x = "Date",
+       y = "Enforcement actions per week",
+      # title = "Weekly enforcement events & detention stay book-ins"
+       color = "Table") +
+  theme_minimal() +
+  theme(legend.position = "bottom")
 
 ggsave(filename=here("figures","weekly-enforcement-events.pdf"), plot=p2, dpi=300, width=8, height=6, units="in")
-
-# ---- Plot of weekly detention stay book-ins ----
-
-# dat <- detention_stays_df %>% 
-#   filter(stay_book_in_date_time >= "2023-09-04",
-#          stay_book_in_date_time <= "2025-07-27") %>% 
-#   mutate(week = floor_date(stay_book_in_date_time, "week", week_start = "Monday")) %>% 
-#   count(week) %>% 
-#   mutate(type = "detention stay book-ins")
-# 
-# p3 <- dat %>% 
-#   ggplot(aes(x = week, y = n, color = type)) +
-#   geom_line() +
-#   ylim(0, NA) +
-#   labs(title = "Weekly detention stay book-ins")
-# 
-# p3
-
-# ---- Comparison across previous releases? ----
 
 # END.
